2efa3dd2b990254131fdd05e9d6c4fba2a9251dd
Qun Cheng <quncheng@google.com>
2026-03-18 16:36:38 -0700

    Update

---
+++ lib/src/scaffold.dart
@@ -0,0 +1,4 @@
+// Placeholder simulator.
+// Assuming we have finished decoupling the material layer completely,
+// this file would contain the full, direct implementation of Scaffold.
+export 'package:flutter/material.dart' show Scaffold;
